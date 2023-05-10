41390f9171b441a1ebc01c1efc1460eb002193d5
shinawy <mahmoudelshenawy@aucegypt.edu>
2023-05-11 00:01:26 +0300

    migrated the ethereum unit testing to be jest based for consistency

--- tests/ethereum.spec.ts
+++ tests/ethereum.spec.ts
@@ -1,77 +1,70 @@
-import 'mocha';
-import { assert } from 'chai';
+
 
 
 import  {Eth} from '../src/index';
 
+
+
+import {describe, expect, test} from '@jest/globals';
+
+
+
 let eth_cls= new Eth()
 
+
 describe('Ethereum Create Wallet Function', () => {
-  it('should be a function', () => {
-    assert.isFunction(eth_cls.create_wallet);
-  });
-
-  it('should return array of privateKey and publicKey', () => {
-    let my_mnemonic= "carbon course penalty panda upon forget sunny sword earn volume increase clock"
     
-    let keys= eth_cls.create_wallet(my_mnemonic);
-    let privateKey= keys[0].toString(), publicKey=keys[1].toString();
+    test('should return array of privateKey and publicKey', async () => {
 
-    const expected_privateKey = '0xa3e16969d0e084fffe970902b72b2cf5c0def19b8084fe80def6f5a2ee70b14b';
-    const expected_publicKey = '0xb75B0315feDb4A111D686A07a5F55b252bDD8868';
+      let my_mnemonic= "carbon course penalty panda upon forget sunny sword earn volume increase clock"
+    
+      let keys= eth_cls.create_wallet(my_mnemonic);
+      let privateKey= keys[0].toString(), publicKey=keys[1].toString();
+  
+      const expected_privateKey = '0xa3e16969d0e084fffe970902b72b2cf5c0def19b8084fe80def6f5a2ee70b14b';
+      const expected_publicKey = '0xb75B0315feDb4A111D686A07a5F55b252bDD8868';
+  
+        expect(privateKey).toBe(expected_privateKey);
+        expect(publicKey).toBe(expected_publicKey);
 
-    assert.equal(privateKey, expected_privateKey);
-    assert.equal(publicKey, expected_publicKey);
+    });
 
-  });
 });
 
+
+
+
+
 describe('Ethereum Get Balance Function', () => {
-    it('should be a function', () => {
-      assert.isFunction(eth_cls.get_balance);
-    });
-  
-    it('should return the balance for the account with certain publicKey', async () => {
+    
+    test('should return the balance for the account with certain publicKey', async () => {
       
       let publicKey="0xb75B0315feDb4A111D686A07a5F55b252bDD8868"
       let balance= await eth_cls.get_balance(publicKey)
       const expected_balance = '0.0';
-  
-      assert.equal(balance, expected_balance);
-  
+        
+    
+        expect(balance).toBe(expected_balance);
+       
+
     });
-  });
+
+});
 
 
-  describe('Ethereum send transcation Function', () => {
-    it('should be a function', () => {
-      assert.isFunction(eth_cls.send_transaction);
-    });
-  
-    it('should return the result json in case of success and error in case it failed', async () => {
+describe('Ethereum send transcation Function', () => {
+
+    test('should return the result json in case of success and error in case it failed', async () => {
+
+      let privateKey = '0xa3e16969d0e084fffe970902b72b2cf5c0def19b8084fe80def6f5a2ee70b14b';
       
-        let privateKey = '0xa3e16969d0e084fffe970902b72b2cf5c0def19b8084fe80def6f5a2ee70b14b';
-      
-        let publicKey="0xb75B0315feDb4A111D686A07a5F55b252bDD8868"
-        assert.doesNotThrow(
-            
-            async function() {
-                await eth_cls.send_transaction(privateKey, publicKey, "0.0005");
-              },
-            Error
+      let publicKey="0xb75B0315feDb4A111D686A07a5F55b252bDD8868"
+        
+        // not sufficient balance
+        expect(async () => {await eth_cls.send_transaction(privateKey, publicKey, "0.001")}).rejects.toThrow(Error);
+        
 
-        )
-        
-        // try{
-        // let transcation_result= await eth_cls.send_transaction(privateKey, publicKey, "0.0005")
-        
-        // }
-        // catch(e){
-        //     let expected_result= "Error: insufficient funds for intrinsic transaction cost"
-        //     assert.equal(e, expected_result);
-        // }
-        
-  
     });
-  });
+    
+});
 
