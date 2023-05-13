1e983940b1ff6b7bc31a9f56d569c2845d1fc964
shinawy <mahmoudelshenawy@aucegypt.edu>
2023-05-13 22:52:34 +0300

    modified the _defaultProvider function by removing the providers parameter as it causes errors in typescript and the code passed the test case without it

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 export {Eth} from "./ethereum/eth_class"
 export {Sol} from "./solana/sol_class"
 export {Cspr} from "./casper/cspr_class"
+export {Polygon} from "./polygon/polygon_class"
+
 export {create_mnemonics} from "./create_mnemonic"
 export {create_seed} from "./create_seed"
 
