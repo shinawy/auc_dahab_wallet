4307ef4019cc497680812cb22d9d8f943cad43f1
shinawy <mahmoudelshenawy@aucegypt.edu>
2023-05-14 15:08:23 +0300

    solved the errors of typing in the libraries in claimKeys and other functions due to json typing problems

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ export {Eth} from "./ethereum/eth_class"
 export {Sol} from "./solana/sol_class"
 export {Cspr} from "./casper/cspr_class"
 export {Polygon} from "./polygon/polygon_class"
+export {MasterWallet} from "./master_wallet/master_wallet"
+
 
 export {create_mnemonics} from "./create_mnemonic"
 export {create_seed} from "./create_seed"
+
 
 export function helloNpm() {
     return "hello NPM"
